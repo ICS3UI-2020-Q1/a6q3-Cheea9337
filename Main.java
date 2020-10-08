3936c98e3549c5285ae99df4f912fbaf5646a769
Cheea9337 <cheea9337@wrdsb.ca>
2020-10-08 15:37:23 +0000

    started a6q3

--- Main.java
+++ Main.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 /**
  *
  * @author 
@@ -9,7 +10,25 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // cretae a Scanner for user input
+    Scanner input = new Scanner(System.in);
+
+    // create an array to store the values
+    int[] numbers = new int[10];
+
+    // ask the user for the 10 numbers
+    System.out.println("Please enter in 10 integers to put into the array");
+
+    // use a for loop to get the values
+    for(int i = 0; i < numbers.length; i++){
+      // fill the array with values
+      numbers[i] = input.nextInt();
+    }
+
+    // use a loop to find the biggest numbers
+    for(int i = 0; i < numbers.length; i++){
+      if(numbers[i] )
+    }
     
   }
 }
